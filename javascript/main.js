6915dde3db703c79635482db6106733a416cbfe8
ShortSoufie <ShortSoufie@gmail.com>
2024-03-14 22:33:23 +0100

    lol

--- javascript/main.js
+++ javascript/main.js
@@ -3,6 +3,7 @@ window.addEventListener('load', init);
 // Globals
 const webservice = 'http://localhost/prg03-eindopdracht/webservice/';
 let gallery;
+let championData = {};
 
 function init() {
     gallery = document.getElementById('league-gallery')
@@ -10,7 +11,7 @@ function init() {
     ajaxRequest(webservice, createLeagueCard);
 }
 
-function ajaxRequest(url, succes) {
+function ajaxRequest(url, success) {
     fetch(url)
         .then((response) => {
             if (!response.ok) {
@@ -18,7 +19,7 @@ function ajaxRequest(url, succes) {
             }
             return response.json();
         })
-        .then(succes)
+        .then(success)
         .catch(ajaxErrorHandler);
 }
 
@@ -26,19 +27,47 @@ function createLeagueCard(data) {
     for (const champion of data) {
         const championCard = document.createElement('div');
         championCard.classList.add('champion-card');
-        championCard.dataset.name = champion.id;
+        championCard.dataset.name = champion.name;
         gallery.appendChild(championCard);
         let championID = champion.id;
 
-        fillChampionCard(championID);
+        fillChampionCard(champion);
     }
 }
 
-function fillChampionCard(data){
-    let troll =
-    console.log();
+function fillChampionCard(champion){
+    const championCard = document.querySelector(`.champion-card[data-name='${champion.name}']`);
 
+    const h2 = document.createElement('h2');
+    h2.innerHTML = `${champion.name} (#${champion.id})`;
+    championCard.appendChild(h2);
+
+    const img = document.createElement('img');
+    img.src = champion.img;
+    img.dataset.championCard = `${champion.name}`;
+    championCard.appendChild(img);
+    img.classList.add('champion-image');
+
+    const btnFav = document.createElement('button');
+    btnFav.innerHTML = `Add to favorites`;
+    btnFav.dataset.id = `${champion.id}`
+    championCard.appendChild(btnFav);
+    btnFav.classList.add('champion-button');
+
+    const btnBio = document.createElement('button');
+    btnBio.innerHTML = `Show Bio`;
+    btnBio.dataset.id = `${champion.id}`
+    championCard.appendChild(btnBio);
+    btnBio.classList.add('champion-button');
+
+    btnBio.addEventListener('click', function() {
+        showChampionBio(champion.id);
+    });
+
+    // Store Champion globally
+    championData[champion.id] = champion;
 }
+
 function ajaxErrorHandler(error) {
     console.error('Ajax request failed:', error);
 }
